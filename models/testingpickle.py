7172e619a2b00eb8812d8d856fbf61a43877fe68
Reuttt2007 <rayhansisfor@gmail.com>
2025-12-02 22:38:45 +0700

    penambahan file uji coba tfidf dan model

--- models/testingpickle.py
+++ models/testingpickle.py
@@ -1,1 +1,24 @@
-import pickle
+import joblib
+import os
+
+# path absolut file
+BASE_PATH = "D:/Ahh miitgasi/Mitigasi_Bencana/models"
+TFIDF_PATH = os.path.join(BASE_PATH, "tfidf_vectorizer.pkl")
+MODEL_PATH = os.path.join(BASE_PATH, "logreg_model.pkl")
+
+# load tfidf
+tfidf = joblib.load(TFIDF_PATH)
+
+# load model
+clf = joblib.load(MODEL_PATH)
+
+# contoh input
+text = ["banjir besar melanda kota sejak dini hari"]
+
+# vectorize
+vec = tfidf.transform(text)
+
+# prediksi
+pred = clf.predict(vec)
+
+print("Prediksi:", pred[0])
